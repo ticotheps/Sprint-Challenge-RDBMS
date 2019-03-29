b4fae8630e62707da2597b6d933f114f8e0f1477
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-29 14:15:59 -0400

    Runs the 'npx knex migrate:make actions_table' command in the terminal to create a new 'actions' table in the database

---
+++ migrations/20190329141501_actions_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('actions', function(tbl) {
+        tbl.increments();
+
+        tbl
+            .string('description', 255)
+            
+        tbl
+            .string('notes', 128)
+            
+        tbl
+            .boolean('completed')
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('actions');
+};
