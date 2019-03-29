0c9b2c71b4e2032c0a73d07ff13a639f89514171
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-29 12:43:30 -0400

    Updates all values in the module.exports object in the 'knexfile.js' file with values from the 'knexConfig' object from projects-router.js

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  }
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+      filename: './data/trackerApp.db3',
+  },
+  debug: true
 };
